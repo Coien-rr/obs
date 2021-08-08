f23a9057ff3dc98134b95524e40f12329be59dfb
Coienrr <972054003@qq.com>
2021-08-08 18:40:49 +0800

    Login page basic framework

---
+++ src/views/login/index.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="login-container">
+        <v-form v-model="loginForm" class="login-form" >
+            <div class="title-container">
+                <h3 class="title1">OBS Login Form</h3>
+            </div>
+            <v-text-field v-model="loginForm.username" label="Username" Required/>
+            <v-text-field v-model="loginForm.password" label="Password" Required/>
+            <v-btn color="success" rounded width="200" style=" display:block; margin: 20px auto">Login</v-btn>
+        </v-form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Login",
+    data(){
+        return {
+            loginForm: {
+                username:'',
+                password:'',
+            },
+            responseResult:[]
+        }
+    },
+    methods:{
+
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    $bg:#2d3a4b;
+    $dark-gary:#889aa4;
+    $light-gray:#eee;
+    .login-container{
+        min-height: 100%;
+        width: 100%;
+        background-color: $light-gray;
+        overflow: hidden;
+
+        .login-form{
+            position: relative;
+            width: 375px;
+            max-height: 100%;
+            padding: 160px 35px 0;
+            margin: 0 auto;
+            overflow: hidden;
+
+            .title-container{
+                position: relative;
+
+                .title1{
+                    font-size: 1.7rem;
+                    color: $bg;
+                    margin: 10px auto 40px auto;
+                    text-align: center;
+                    font-weight: bold;
+                }
+            }
+        }
+
+
+
+
+
+    }
+
+</style>

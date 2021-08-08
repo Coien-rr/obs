9a115d62d271cdb06ff4f42e38aaf9d17b192e7e
Coienrr <972054003@qq.com>
2021-08-08 22:56:16 +0800

    Login page basic framework

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,11 +2,11 @@
     <div class="login-container">
         <v-form v-model="loginForm" class="login-form" >
             <div class="title-container">
-                <h3 class="title1">OBS Login Form</h3>
+                <h3 class="title1">OBS Login</h3>
             </div>
             <v-text-field v-model="loginForm.username" label="Username" Required/>
             <v-text-field v-model="loginForm.password" label="Password" Required/>
-            <v-btn color="success" rounded width="200" style=" display:block; margin: 20px auto">Login</v-btn>
+            <v-btn color="success" rounded width="200" style=" display:block; margin: 20px auto" v-on:click="login">Login</v-btn>
         </v-form>
     </div>
 </template>
@@ -24,7 +24,25 @@ export default {
         }
     },
     methods:{
-
+        login() {
+            let _this = this
+            this.$axios
+                .get('https://qc5wyz.fn.thelarkcloud.com/loginVerify',{
+                    param: {
+                        username: this.loginForm.username,
+                        password: this.loginForm.password
+                    }
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+                .then((response) => {
+                    console.log(response);
+                    if(response.data.password === _this.loginForm.password){
+                        this.$router.replace({name: 'Home'})
+                    }
+                })
+        }
     }
 }
 </script>
